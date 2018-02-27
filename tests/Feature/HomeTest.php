5644f754dbbd24cf5865fa0400567ba7732acf4a
Simon W <mister_simon@hotmail.co.uk>
2018-02-27 17:48:10 +0000

    Updating tests

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -5,14 +5,14 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class HomeTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testHomePageLoads()
     {
         $response = $this->get('/');
 
