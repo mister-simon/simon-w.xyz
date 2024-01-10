ce42c2447b4114e674937484317675e36fac9c2b
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-01-10 16:36:40 +0000

    Final setup - this actually works how I want it to. Good.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: {
+    enabled: true,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  build: {
+    postcss: {
+      plugins: {
+        "postcss-import": {},
+        "tailwindcss/nesting": {},
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+});
