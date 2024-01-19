dc95c7704f13412b841221a8273035a193938b77
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-01-19 11:34:19 +0000

    Use more comprehensive site manifest and generated icons.

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,48 @@
 useHead({
   link: [
     {
-      rel: 'icon',
-      href: 'favicon.png'
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png"
     },
     {
-      rel: 'icon',
-      href: 'favicon.svg',
-      type: 'image/svg+xml'
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png"
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png"
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest"
+    },
+    {
+      rel: "mask-icon",
+      href: "/safari-pinned-tab.svg",
+      color: "#5bbad5"
+    }
+  ],
+  meta: [
+    {
+      name: "apple-mobile-web-app-title",
+      content: "simonw.xyz"
+    },
+    {
+      name: "application-name",
+      content: "simonw.xyz"
+    },
+    {
+      name: "msapplication-TileColor",
+      content: "#2b5797"
+    },
+    {
+      name: "theme-color",
+      content: "#ffffff"
     }
   ]
 })
