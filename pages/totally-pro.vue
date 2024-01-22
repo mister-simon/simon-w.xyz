69c7d96fbc4b8b245d38227ede1f657969da40c0
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-01-22 17:15:50 +0000

    Lots more updates to totally pro blah

--- pages/totally-pro.vue
+++ pages/totally-pro.vue
@@ -5,6 +5,10 @@ let obs = null;
 const cb = function (entries) {
     entries.forEach(
         ({ isIntersecting, target }) => {
+            const figure = target.previousElementSibling;
+            if (figure) {
+                figure.classList.toggle('active', isIntersecting)
+            }
             target.classList.toggle('active', isIntersecting)
         }
     );
@@ -29,7 +33,21 @@ onMounted(() => {
     </nav>
 
     <section class="bg-black text-neutral-100 isolate" ref="section">
-        <figure class="transition duration-500 bg-neutral-600 overflow-clip">
+        <figure class="group overflow-clip bg-neutral-900">
+            <div
+                class="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-full transition delay-1000 duration-1000 group-[.active]:-translate-x-1/2">
+                <NuxtImg src="/assets/tedtalk-emphasis.png" class="rotate-12 brightness-50 -scale-x-100" />
+            </div>
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <p class="text-sm-relative">
+                    <em>*clapping*</em>
+                </p>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-600 overflow-clip">
             <NuxtImg src="/assets/tedtalk-emphasis.png" />
         </figure>
         <div class="outer">
@@ -40,7 +58,7 @@ onMounted(() => {
             </div>
         </div>
 
-        <figure class="transition duration-500 bg-neutral-600">
+        <figure class="group bg-neutral-600">
             <NuxtImg src="/assets/tedtalk-neutral.png" />
         </figure>
         <div class="outer">
@@ -52,8 +70,9 @@ onMounted(() => {
         </div>
 
 
-        <figure class="transition duration-500 bg-blue-900">
-            <NuxtImg src="/assets/tedtalk-emphasis.png" class="rotate-45" />
+        <figure class="group bg-blue-900">
+            <NuxtImg src="/assets/tedtalk-blocky.png"
+                class="transition delay-1000 group-[.active]:delay-500 -scale-x-100 -translate-x-4 group-[.active]:rotate-45" />
         </figure>
         <div class="outer">
             <div class="inner">
@@ -67,45 +86,211 @@ onMounted(() => {
         </div>
 
 
-        <figure class="transition duration-500 bg-neutral-700">
-            <NuxtImg src="/assets/tedtalk-blocky.png" />
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-neutral.png" />
         </figure>
         <div class="outer">
             <div class="inner">
                 <blockquote>
-                    <p>Heh... No.</p>
+                    <p>Hehe... No.</p>
                 </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-neutral.png" class=" -scale-x-100" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>Though experience helps, being a professional is about holding yourself to a standard.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" class="-scale-x-100" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>You might be making a product for a client, but you're also making it for every single user that
+                        might interact with that product.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-extra-emphasis.png" class="-scale-x-100" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>You're making something that will pass through the hands of your colleagues, contractors, your future
+                        self...</p>
+                </blockquote>
+                <p class="text-sm-relative">
+                    <em>*several people in the crowd feel called out, chuckling nervously*</em>
+                </p>
+            </div>
+        </div>
+
+        <figure class="group bg-red-950">
+            <NuxtImg src="/assets/tedtalk-neutral.png" class="" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>You even have to have an awareness of the laws affecting your product.</p>
+                </blockquote>
+                <p class="text-sm-relative">
+                    <em>*several people in the crowd grow silent, even more nervously*</em>
+                </p>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-a-dog.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>Being a professional is about understanding that you have the power to make something good if you
+                        present your skills well - you take responsibility for scoping, fulfil requirements, and fight for
+                        quality within any known limitations.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-a-dog-bork.png" class="motion-safe:animate-bounce" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>You do your best to plan ahead, you execute, you evaluate, refactor, maintain, and you remain open to
+                        criticism and growth throughout.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>People consider professionalism as an aesthetic.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>A suit and tie - a suitcase full of clean cut black and white business cards.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>Hopefully that's been less important to you since 2022.</p>
+                </blockquote>
+                <p class="text-sm-relative">
+                    <em>*a single micro-tear falls from a micro-manager's eye in the audience*</em>
+                </p>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>Granted, I look good in a suit.</p>
+                </blockquote>
+                <p class="text-sm-relative">
+                    <em>*the crowd chuckles agreeably, one person whoops then coughs sheepishly*</em>
+                </p>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>Thanks... But if you take one thing away today, it's that professionalism is approaching your craft
+                        with integrity and following through.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+            <NuxtImg src="/assets/tedtalk-emphasis.png" />
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <blockquote>
+                    <p>Thank you for coming to my TEDTalk.</p>
+                </blockquote>
+            </div>
+        </div>
+
+        <figure class="group bg-neutral-700">
+        </figure>
+        <div class="outer">
+            <div class="inner">
+                <p class="text-sm-relative">
+                    <em>*clapping*</em>
+                </p>
             </div>
         </div>
     </section>
 </template>
 
 <style>
+.text-sm-relative {
+    font-size: .5em;
+}
+
 section {
-    /* Layout */
     display: grid;
     text-align: center;
 
-    /* Children */
-    div,
+    .outer,
     figure {
         display: grid;
         font-size: clamp(2rem, 3vw, 10rem);
     }
 
-    .text-sm-relative {
-        font-size: .5em;
+    figure {
+        @apply transition duration-1000;
+        @apply fixed inset-0;
+        @apply place-content-center;
+        @apply bg-gradient-to-b from-[transparent_70%] to-[#0009_70%];
+
+        &.no-horizon {
+            background-image: none;
+        }
     }
 
     .outer {
         min-height: 90vh;
         min-height: 90svh;
 
-        position: relative;
-        z-index: 20;
-        display: grid;
-        align-items: center;
-        gap: 1rem;
+        @apply relative z-20;
+        @apply grid items-center gap-4;
 
         blockquote {
             & p:first-of-type::before {
@@ -116,35 +301,28 @@ section {
                 content: '”';
             }
         }
-    }
 
-    .inner {
-        @apply bg-black/70 rounded-lg;
-        margin: clamp(.2rem, 2%, 1rem);
-        padding: clamp(.2rem, 2%, 1rem);
-    }
-
-    figure {
-        position: fixed;
-        inset: 0;
-        place-content: center;
+        .inner {
+            @apply bg-black/90 rounded-lg;
+            margin: clamp(.2rem, 2%, 1rem);
+            padding: clamp(.2rem, 2%, 1rem);
+        }
     }
 
     @screen sm {
         grid-template-columns: 1fr 2fr;
 
+        figure {
+            @apply sticky;
+        }
+
         .outer {
             min-height: 100vh;
             min-height: 100svh;
-        }
 
-        .inner {
-            z-index: 0;
-            @apply bg-transparent;
-        }
-
-        figure {
-            position: sticky;
+            .inner {
+                @apply bg-transparent z-0;
+            }
         }
     }
 }
@@ -153,13 +331,10 @@ section {
 section {
     figure {
         /* Allow slides to show / hide when activated */
-        @apply transition-opacity duration-500;
-        opacity: 0;
+        @apply transition duration-1000 delay-1000 opacity-0 overflow-clip;
 
-        &:has(+.active) {
-            /* @apply delay-100; */
-            opacity: 1;
-            z-index: 10;
+        &.active {
+            @apply delay-0 z-10 opacity-100 overflow-visible;
         }
     }
 }
