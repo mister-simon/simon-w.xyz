a8abf5131b02f3220a643985c7a5decd585764ee
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-06-11 16:40:02 +0100

    Let's tweak the homepage and see what happens

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
-    <Body class="bg-black text-neutral-100 grid min-h-svh" />
-    <div class="grid min-h-full">
 
+    <Body class="bg-black text-neutral-100 grid min-h-svh" />
+
+    <div class="grid min-h-full">
         <slot />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
+
   <Head>
     <Title>Welcome! | SimonW.XYZ</Title>
   </Head>
-  <div class="place-self-center">
-    <h1 class="text-3xl">What are you looking for?</h1>
+
+  <div class="place-self-center bg-gradient-to-br from-slate-950 to-transparent">
+    <h1>Well Hello There!</h1>
+    <p>Welcome to SimonW.xyz...</p>
+    <p><strong>Simon W</strong> is a <strong>Web Developer</strong> - and has been since he was about 14 years old
+      (roughly {{ (new Date).getFullYear() - 2007 }} years)</p>
+    <p>
+      <NuxtLink to="/thoughts-and-things">I have some thoughts and things... Read 'em!</NuxtLink>
+    </p>
+
+    <h2 class="text-3xl">Some other things?</h2>
     <ul>
       <!-- <li>
         <NuxtLink to="/is-a-salmon">Simon W</NuxtLink>
@@ -12,13 +22,10 @@
         <NuxtLink to="/is-smrt">Knowledge</NuxtLink>
       </li> -->
       <li>
-        <NuxtLink to="/makes-incredible-designs">An Incredible Designer</NuxtLink>
-      </li>
-      <li>
         <NuxtLink to="/totally-pro">A Professional</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/thoughts-and-things">Thoughts... And... Things?</NuxtLink>
+        <NuxtLink to="/makes-incredible-designs">An Incredible Designer</NuxtLink>
       </li>
     </ul>
   </div>
