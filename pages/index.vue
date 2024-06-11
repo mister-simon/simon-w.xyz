e352e4c2e2558791265ce0db6f1e9ba28d3dc5ca
Simon W <4751279+mister-simon@users.noreply.github.com>
2024-06-11 17:18:04 +0100

    Some homepage stuff

--- pages/index.vue
+++ pages/index.vue
@@ -4,29 +4,53 @@
     <Title>Welcome! | SimonW.XYZ</Title>
   </Head>
 
-  <div class="place-self-center bg-gradient-to-br from-slate-950 to-transparent">
-    <h1>Well Hello There!</h1>
-    <p>Welcome to SimonW.xyz...</p>
-    <p><strong>Simon W</strong> is a <strong>Web Developer</strong> - and has been since he was about 14 years old
-      (roughly {{ (new Date).getFullYear() - 2007 }} years)</p>
-    <p>
-      <NuxtLink to="/thoughts-and-things">I have some thoughts and things... Read 'em!</NuxtLink>
-    </p>
+  <div class="place-self-center relative p-10">
+    <div class="relative z-10 prose prose-invert">
+      <h1>simonw.xyz</h1>
+      <p class="lead"><strong>Simon W</strong> is a <strong>Web Developer</strong> and has been since he was about 14
+        years old... About <em>{{ (new Date).getFullYear() - 2007 }} years ago</em>.</p>
 
-    <h2 class="text-3xl">Some other things?</h2>
-    <ul>
-      <!-- <li>
-        <NuxtLink to="/is-a-salmon">Simon W</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/is-smrt">Knowledge</NuxtLink>
-      </li> -->
-      <li>
-        <NuxtLink to="/totally-pro">A Professional</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/makes-incredible-designs">An Incredible Designer</NuxtLink>
-      </li>
-    </ul>
+      <p>
+        I usually work with Laravel in PHP - but also I:
+      <ul>
+        <li class="text-[1.4em]">
+          <NuxtLink to="/thoughts-and-things">Have thoughts and things... Read 'em!</NuxtLink>
+        </li>
+        <li>Work with Vue and learnt made this site partially to learn Nuxt.</li>
+        <li>Am learning some React / Next.js</li>
+        <li>Played Dustforce on a charity livestream with thousands of viewers</li>
+        <li>Used to make flash animations</li>
+        <li>Have a wonderful fiancee and adorable two rabbits</li>
+        <li>Use Pop!_OS</li>
+        <li>Use VSCode</li>
+        <li>Learnt the Colemak keyboard layout and switched to a 40% ortho keyboard... Easily the nerdiest thing I've
+          done in recent years.
+        </li>
+      </ul>
+      </p>
+
+      <h2>Some other nonsense:</h2>
+      <p>
+      <ul>
+        <li>
+          <NuxtLink to="/totally-pro">What is a Professional?</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/makes-incredible-designs">Is Simon an Incredible Designer?</NuxtLink>
+        </li>
+      </ul>
+      </p>
+    </div>
+  </div>
+
+  <div class="absolute inset-0 z-0 overflow-hidden">
+    <NuxtImg src="favicon.svg"
+      class="object-contain size-full text-slate-800 brightness-[.33] motion-safe:animate-spin [--animate-duration:60s] blur-3xl bg-offset" />
   </div>
 </template>
+
+<style scoped>
+.bg-offset {
+  translate: -10% -50%;
+}
+</style>
